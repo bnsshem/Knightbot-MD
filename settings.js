939a55d88cacca69a7c52deafcf6b5fcd4b33fc8
bnsshem <bnssharif@gmail.com>
2025-07-03 15:41:53 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Benito",
   botOwner: 'Benito', // Your name
-  ownerNumber: '+254743643222', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '254743643222', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
